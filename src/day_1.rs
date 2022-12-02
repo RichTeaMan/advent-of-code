79a51dfc996893d980d81747b4ca8113a756aba9
Thomas Holmes <thomasholmes@outlook.com>
2022-12-02 13:43:41 +0000

    Simplified day 1. Restored part 1 answer.

--- src/day_1.rs
+++ src/day_1.rs
@@ -3,39 +3,45 @@ use std::io::{self};
 use crate::file_utils::read_lines;
 
 /**
- * Gets the sum of the 3 highest calorie counts from day-1-input.txt.
+ * Gets the highest calorie count from day-1-input.txt.
  */
 pub fn day_1() -> io::Result<i32> {
-    let mut max_calories_1 = 0;
-    let mut max_calories_2 = 0;
-    let mut max_calories_3 = 0;
-    // File hosts must exist in current path before this produces output
+
+    let mut calories = fetch_calories()?;
+    calories.sort();
+    calories.reverse();
+    Ok(*calories.first().unwrap())
+}
+
+/**
+ * Gets the sum of the 3 highest calorie counts from day-1-input.txt.
+ */
+pub fn day_1_part_2() -> io::Result<i32> {
+
+    let mut calories = fetch_calories()?;
+    calories.sort();
+    calories.reverse();
+    Ok(calories.iter().take(3).sum::<i32>())
+}
+
+fn fetch_calories() -> io::Result<Vec<i32>> {
+
+    let mut calories_vec = Vec::new();
+
     let lines = read_lines("./day-1-input.txt")?;
     let mut calories = 0;
-    // Consumes the iterator, returns an (Optional) String
     for line_opt in lines {
         if let Ok(line) = line_opt {
             let line_calories_opt = line.parse::<i32>();
             if let Ok(line_calories) = line_calories_opt {
                 calories += line_calories;
             } else if line.is_empty() {
-                if calories > max_calories_1 {
-                    max_calories_3 = max_calories_2;
-                    max_calories_2 = max_calories_1;
-                    max_calories_1 = calories;
-                } else if calories > max_calories_2 {
-                    max_calories_3 = max_calories_2;
-                    max_calories_2 = calories;
-                } else if calories > max_calories_3 {
-                    max_calories_3 = calories;
-                }
-
+                calories_vec.push(calories);
                 calories = 0;
             }
         }
     }
-    let answer = max_calories_1 + max_calories_2 + max_calories_3;
-    Ok(answer)
+    Ok(calories_vec)
 }
 
 #[cfg(test)]
@@ -45,6 +51,11 @@ mod tests {
 
     #[test]
     fn test() {
-        assert_eq!(day_1().unwrap(), 206582);
+        assert_eq!(day_1().unwrap(), 70116);
+    }
+
+    #[test]
+    fn part_2_test() {
+        assert_eq!(day_1_part_2().unwrap(), 206582);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,12 @@ mod day_1;
 mod day_2;
 mod file_utils;
 
-use crate::day_1::day_1;
+use crate::day_1::{day_1, day_1_part_2};
 use crate::day_2::{day_2, day_2_part_2};
 
 fn main() {
-    println!("Day 1 part 2 answer: {answer}", answer = day_1().unwrap());
+    println!("Day 1 part 1 answer: {answer}", answer = day_1().unwrap());
+    println!("Day 1 part 2 answer: {answer}", answer = day_1_part_2().unwrap());
     println!("Day 2 part 1 answer: {answer}", answer = day_2().unwrap());
     println!(
         "Day 2 part 2 answer: {answer}",
