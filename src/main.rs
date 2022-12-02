638fb788b29d2c2dab35e4b0be5a9e7f485f3e58
Thomas Holmes <thomasholmes@outlook.com>
2022-12-02 12:23:59 +0000

    Main now prints all answers.

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,14 @@ mod day_1;
 mod day_2;
 mod file_utils;
 
+use crate::day_1::day_1;
 use crate::day_2::{day_2, day_2_part_2};
 
 fn main() {
-    let answer = day_2_part_2().unwrap();
-    println!("Answer: {answer}");
+    println!("Day 1 part 2 answer: {answer}", answer = day_1().unwrap());
+    println!("Day 2 part 1 answer: {answer}", answer = day_2().unwrap());
+    println!(
+        "Day 2 part 2 answer: {answer}",
+        answer = day_2_part_2().unwrap()
+    );
 }
