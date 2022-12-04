a124ba60fef27d80f6cc4b161bdf50c2fb194a82
Thomas Holmes <thomasholmes@outlook.com>
2022-12-04 13:14:18 +0000

    Day 4 part 2 solution.

--- src/day_4.rs
+++ src/day_4.rs
@@ -24,10 +24,19 @@ impl ElfRange {
     pub fn contains(&self, other: &Self) -> bool {
         self.start <= other.start && self.end >= other.end
     }
+
+    pub fn overlap(&self, other: &Self) -> bool {
+        (self.start >= other.start && self.start <= other.end)
+            || (self.end >= other.start && self.end <= other.end)
+    }
 }
 
 pub fn day_4() -> io::Result<i32> {
     fetch_overlapping_pairs("day-4-input.txt")
+}
+
+pub fn day_4_part_2() -> io::Result<i32> {
+    fetch_overlapping_ranges("day-4-input.txt")
 }
 
 fn fetch_overlapping_pairs(filename: &str) -> io::Result<i32> {
@@ -52,6 +61,28 @@ fn fetch_overlapping_pairs(filename: &str) -> io::Result<i32> {
     Ok(overlapping_pairs)
 }
 
+fn fetch_overlapping_ranges(filename: &str) -> io::Result<i32> {
+    let mut overlapping_pairs = 0;
+
+    let lines = read_lines(filename)?;
+    for line in lines.flatten() {
+        if line.is_empty() {
+            continue;
+        }
+        if let Some((a, b)) = line.split(',').collect_tuple() {
+            let range_a = ElfRange::from_str(a);
+            let range_b = ElfRange::from_str(b);
+
+            if range_a.overlap(&range_b) || range_b.overlap(&range_a) {
+                overlapping_pairs += 1;
+            }
+        } else {
+            panic!("irregular comma");
+        }
+    }
+    Ok(overlapping_pairs)
+}
+
 #[cfg(test)]
 mod tests {
 
@@ -59,11 +90,27 @@ mod tests {
 
     #[test]
     fn small_test() {
-        assert_eq!(fetch_overlapping_pairs("./day-4-input-test.txt").unwrap(), 2);
+        assert_eq!(
+            fetch_overlapping_pairs("./day-4-input-test.txt").unwrap(),
+            2
+        );
     }
-    
+
     #[test]
     fn test() {
         assert_eq!(fetch_overlapping_pairs("./day-4-input.txt").unwrap(), 540);
+    }
+
+    #[test]
+    fn small_test_part_2() {
+        assert_eq!(
+            fetch_overlapping_ranges("./day-4-input-test.txt").unwrap(),
+            4
+        );
+    }
+
+    #[test]
+    fn test_part_2() {
+        assert_eq!(fetch_overlapping_ranges("./day-4-input.txt").unwrap(), 540);
     }
 }
