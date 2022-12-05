45ddd52c7f594bf029f072399a87193cad0294cc
Thomas Holmes <thomasholmes@outlook.com>
2022-12-05 12:27:27 +0000

    Added day 5 solution.

---
+++ src/day_5.rs
@@ -0,0 +1,144 @@
+use std::{
+    collections::VecDeque,
+    io::{self},
+};
+
+use itertools::Itertools;
+
+use crate::file_utils::read_lines;
+
+struct CraneStacks {
+    pub stacks: Vec<VecDeque<char>>,
+}
+
+impl CraneStacks {
+    fn read_top_letters(&self) -> String {
+        let mut result = "".to_string();
+        for stack in &self.stacks {
+            result += &stack[0].to_string();
+        }
+        result
+    }
+}
+
+pub fn day_5() -> io::Result<String> {
+    let path = "./day-5-input.txt";
+    let mut crane_stacks = fetch_starting_stacks(path)?;
+    crane_stacks = execute_crane_instructions(path, crane_stacks)?;
+
+    Ok(crane_stacks.read_top_letters())
+}
+
+pub fn day_5_part_2() -> io::Result<i32> {
+    todo!();
+}
+
+fn fetch_starting_stacks(filepath: &str) -> io::Result<CraneStacks> {
+    let mut stacks: Vec<VecDeque<char>> = Vec::new();
+    let lines = read_lines(filepath)?;
+    for line in lines.flatten() {
+        if !line.contains('[') {
+            break;
+        }
+
+        let mut current_stack = 0;
+        for stack_ele in line
+            .chars()
+            .chunks(4)
+            .into_iter()
+            .map(|chunk| chunk.collect::<String>())
+            .collect::<Vec<String>>()
+        {
+            if stacks.len() <= current_stack {
+                stacks.push(VecDeque::new());
+            }
+
+            if !&stack_ele.trim().is_empty() {
+                let trim: &[_] = &['[', ']', ' '];
+                let trimmed = stack_ele.trim_matches(trim).to_string();
+
+                assert!(!trimmed.is_empty());
+                let stack_opt = stacks.get_mut(current_stack);
+                stack_opt
+                    .unwrap()
+                    .push_back(trimmed.chars().next().unwrap());
+            }
+
+            current_stack += 1;
+        }
+    }
+    Ok(CraneStacks { stacks })
+}
+
+fn execute_crane_instructions(
+    filepath: &str,
+    mut crane_stacks: CraneStacks,
+) -> io::Result<CraneStacks> {
+    let lines = read_lines(filepath)?;
+    for line in lines.flatten() {
+        if !line.contains("move") {
+            continue;
+        }
+
+        // eg move 1 from 2 to 1
+        if let Some((_move, crate_count_s, _from, start_stack_id_s, _to, end_stack_id_s)) =
+            line.split_whitespace().collect_tuple()
+        {
+            let start_stack_id: usize = str::parse::<usize>(start_stack_id_s).unwrap() - 1;
+            let end_stack_id: usize = str::parse::<usize>(end_stack_id_s).unwrap() - 1;
+            let crate_count: i32 = str::parse(crate_count_s).unwrap();
+
+            for _ in 0..crate_count {
+
+                let crane_crate = crane_stacks
+                    .stacks
+                    .get_mut(start_stack_id)
+                    .unwrap()
+                    .pop_front()
+                    .unwrap();
+                crane_stacks
+                    .stacks
+                    .get_mut(end_stack_id)
+                    .unwrap()
+                    .push_front(crane_crate);
+            }
+        } else {
+            panic!("Unexpected string format: {line}");
+        }
+    }
+    Ok(crane_stacks)
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn fetch_starting_stacks_small_test() {
+        assert_eq!(
+            fetch_starting_stacks("./day-5-input-test.txt")
+                .unwrap()
+                .read_top_letters(),
+            "NDP"
+        );
+    }
+
+    #[test]
+    fn execute_crane_instructions_small_test() {
+        let mut crane_stacks = fetch_starting_stacks("./day-5-input-test.txt").unwrap();
+        crane_stacks = execute_crane_instructions("./day-5-input-test.txt", crane_stacks).unwrap();
+        assert_eq!(crane_stacks.read_top_letters(), "CMZ");
+    }
+
+    #[test]
+    fn execute_crane_instructions_test() {
+        let mut crane_stacks = fetch_starting_stacks("./day-5-input.txt").unwrap();
+        crane_stacks = execute_crane_instructions("./day-5-input.txt", crane_stacks).unwrap();
+        assert_eq!(crane_stacks.read_top_letters(), "FRDSQRRCD");
+    }
+
+    #[test]
+    fn part_2_test() {
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,14 @@ mod day_1;
 mod day_2;
 mod day_3;
 mod day_4;
+mod day_5;
 mod file_utils;
 
 use crate::day_1::{day_1, day_1_part_2};
 use crate::day_2::{day_2, day_2_part_2};
 use crate::day_3::{day_3, day_3_part_2};
 use crate::day_4::{day_4, day_4_part_2};
+use crate::day_5::day_5;
 
 fn main() {
     println!("Day 1 part 1 answer: {answer}", answer = day_1().unwrap());
@@ -30,4 +32,5 @@ fn main() {
         "Day 4 part 2 answer: {answer}",
         answer = day_4_part_2().unwrap()
     );
+    println!("Day 5 part 1 answer: {answer}", answer = day_5().unwrap());
 }
