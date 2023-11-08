3b382575de6d6fff37a6fc0d781041a72866cbfd
Thomas Holmes <thomasholmes@outlook.com>
2023-11-08 17:58:03 +0000

    Aligned console output.

--- 2021/src/main.rs
+++ 2021/src/main.rs
@@ -29,49 +29,49 @@ fn main() {
     println!("Advent of Code 2021: https://adventofcode.com/2021");
     println!();
 
-    println!("Day 1 part 1 answer: {answer}", answer = day_1().unwrap());
+    println!("Day  1 part 1 answer: {answer}", answer = day_1().unwrap());
     println!(
-        "Day 1 part 2 answer: {answer}",
+        "Day  1 part 2 answer: {answer}",
         answer = day_1_part_2().unwrap()
     );
-    println!("Day 2 part 1 answer: {answer}", answer = day_2().unwrap());
+    println!("Day  2 part 1 answer: {answer}", answer = day_2().unwrap());
     println!(
-        "Day 2 part 2 answer: {answer}",
+        "Day  2 part 2 answer: {answer}",
         answer = day_2_part_2().unwrap()
     );
-    println!("Day 3 part 1 answer: {answer}", answer = day_3().unwrap());
+    println!("Day  3 part 1 answer: {answer}", answer = day_3().unwrap());
     println!(
-        "Day 3 part 2 answer: {answer}",
+        "Day  3 part 2 answer: {answer}",
         answer = day_3_part_2().unwrap()
     );
-    println!("Day 4 part 1 answer: {answer}", answer = day_4().unwrap());
+    println!("Day  4 part 1 answer: {answer}", answer = day_4().unwrap());
     println!(
-        "Day 4 part 2 answer: {answer}",
+        "Day  4 part 2 answer: {answer}",
         answer = day_4_part_2().unwrap()
     );
-    println!("Day 5 part 1 answer: {answer}", answer = day_5().unwrap());
+    println!("Day  5 part 1 answer: {answer}", answer = day_5().unwrap());
     println!(
-        "Day 5 part 2 answer: {answer}",
+        "Day  5 part 2 answer: {answer}",
         answer = day_5_part_2().unwrap()
     );
-    println!("Day 6 part 1 answer: {answer}", answer = day_6().unwrap());
+    println!("Day  6 part 1 answer: {answer}", answer = day_6().unwrap());
     println!(
-        "Day 6 part 2 answer: {answer}",
+        "Day  6 part 2 answer: {answer}",
         answer = day_6_part_2().unwrap()
     );
-    println!("Day 7 part 1 answer: {answer}", answer = day_7().unwrap());
+    println!("Day  7 part 1 answer: {answer}", answer = day_7().unwrap());
     println!(
-        "Day 7 part 2 answer: {answer}",
+        "Day  7 part 2 answer: {answer}",
         answer = day_7_part_2().unwrap()
     );
-    println!("Day 8 part 1 answer: {answer}", answer = day_8().unwrap());
+    println!("Day  8 part 1 answer: {answer}", answer = day_8().unwrap());
     println!(
-        "Day 8 part 2 answer: {answer}",
+        "Day  8 part 2 answer: {answer}",
         answer = day_8_part_2().unwrap()
     );
-    println!("Day 9 part 1 answer: {answer}", answer = day_9().unwrap());
+    println!("Day  9 part 1 answer: {answer}", answer = day_9().unwrap());
     println!(
-        "Day 9 part 2 answer: {answer}",
+        "Day  9 part 2 answer: {answer}",
         answer = day_9_part_2().unwrap()
     );
     println!("Day 10 part 1 answer: {answer}", answer = day_10().unwrap());
