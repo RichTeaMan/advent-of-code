5a67bbd04c497b5ba6128c681896aa1d937f83bc
Thomas Holmes <thomasholmes@outlook.com>
2023-12-23 15:21:55 +0000

    Finally solved 2021 day 21 part 2.

--- 2021/src/day_21.rs
+++ 2021/src/day_21.rs
@@ -1,12 +1,13 @@
-use std::io;
+use std::{collections::HashMap, io};
+
 
 use utils::file_utils::read_lines;
 
 pub fn day_21() -> io::Result<i32> {
     eval_game_from_file("./inputs/day-21-input.txt")
 }
-pub fn day_21_part_2() -> io::Result<i64> {
-    panic!() //find_largest_magnitude_from_file("./inputs/day-21-input.txt")
+pub fn day_21_part_2() -> io::Result<u128> {
+    eval_dirac_game_from_file("./inputs/day-21-input.txt")
 }
 
 #[derive(Debug)]
@@ -25,16 +26,37 @@ impl DeterministicDice {
 
     fn roll(&mut self) -> i32 {
         self.last_roll += 1;
-        self.last_roll = self.last_roll % 100;
+        self.last_roll %= 100;
         self.roll_count += 1;
         self.last_roll
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy, Hash, Eq, PartialEq)]
 struct Player {
     score: i32,
     position: i32,
+}
+
+#[derive(Debug, Clone, Copy, Hash, Eq, PartialEq)]
+struct GameState {
+    player_1: Player,
+    player_2: Player,
+}
+
+impl GameState {
+    fn from_positions(player_1_position: i32, player_2_position: i32) -> Self {
+        GameState {
+            player_1: Player {
+                position: player_1_position,
+                score: 0,
+            },
+            player_2: Player {
+                position: player_2_position,
+                score: 0,
+            },
+        }
+    }
 }
 
 fn eval_game_from_file(filename: &str) -> io::Result<i32> {
@@ -51,14 +73,13 @@ fn eval_game(mut players: Vec<Player>) -> i32 {
 
     let winning_score = 1000;
 
-    while &players.iter().map(|p| p.score).max().unwrap() < &winning_score {
+    while players.iter().map(|p| p.score).max().unwrap() < winning_score {
         // roll three times
         let roll = dice.roll() + dice.roll() + dice.roll();
 
-        let mut player = players.get_mut(player_index).unwrap();
-        //.position;
+        let player = players.get_mut(player_index).unwrap();
         let new_pos = ((roll + (player.position - 1)) % 10) + 1;
-        debug_assert!(new_pos >= 1 && new_pos <= 10);
+        debug_assert!((1..=10).contains(&new_pos));
 
         player.position = new_pos;
         player.score += new_pos;
@@ -66,7 +87,7 @@ fn eval_game(mut players: Vec<Player>) -> i32 {
         player_index += 1;
         player_index %= player_count;
     }
-    &players.iter().map(|p| p.score).min().unwrap() * dice.roll_count
+    players.iter().map(|p| p.score).min().unwrap() * dice.roll_count
 }
 
 fn load_players(filename: &str) -> io::Result<Vec<Player>> {
@@ -82,6 +103,97 @@ fn load_players(filename: &str) -> io::Result<Vec<Player>> {
         players.push(Player { position, score: 0 });
     }
     Ok(players)
+}
+
+fn eval_dirac_game_from_file(filename: &str) -> io::Result<u128> {
+    let players = load_players(filename)?;
+    if players.len() != 2 {
+        panic!(
+            "Irregular number of players: {}. Expected 2.",
+            players.len()
+        );
+    }
+
+    let player_1_position = players.first().unwrap().position;
+    let player_2_position = players.get(1).unwrap().position;
+
+    Ok(eval_dirac_game(player_1_position, player_2_position))
+}
+
+fn eval_dirac_game(player_1_position: i32, player_2_position: i32) -> u128 {
+    let mut combos: HashMap<i32, u128> = HashMap::new();
+    // pre calc all the combos of 3 dice
+    for a in 1..=3 {
+        for b in 1..=3 {
+            for c in 1..=3 {
+                let total = a + b + c;
+                combos.entry(total).and_modify(|e| *e += 1).or_insert(1);
+            }
+        }
+    }
+
+    let mut game_states = HashMap::new();
+    game_states.insert(
+        GameState::from_positions(player_1_position, player_2_position),
+        1_u128,
+    );
+
+    let mut p_1_wins = 0_u128;
+    let mut p_2_wins = 0_u128;
+
+    while !game_states.is_empty() {
+        // process player 1
+        let mut new_game_states = HashMap::new();
+        for (state, count) in &game_states {
+            for (roll, roll_count) in &combos {
+                let mut new_state = *state;
+
+                let new_pos = ((roll + (state.player_1.position - 1)) % 10) + 1;
+                debug_assert!((1..=10).contains(&new_pos));
+
+                new_state.player_1.position = new_pos;
+                new_state.player_1.score += new_pos;
+                let instances: u128 = count * roll_count;
+
+                if new_state.player_1.score >= 21 {
+                    p_1_wins += instances;
+                } else {
+                    new_game_states
+                        .entry(new_state)
+                        .and_modify(|e| *e += instances)
+                        .or_insert(instances);
+                }
+            }
+        }
+        game_states = new_game_states;
+
+        // process player 2
+        let mut new_game_states = HashMap::new();
+        for (state, count) in &game_states {
+            for (roll, roll_count) in &combos {
+                let mut new_state = *state;
+
+                let new_pos = ((roll + (state.player_2.position - 1)) % 10) + 1;
+                debug_assert!((1..=10).contains(&new_pos));
+
+                new_state.player_2.position = new_pos;
+                new_state.player_2.score += new_pos;
+                let instances: u128 = count * roll_count;
+
+                if new_state.player_2.score >= 21 {
+                    p_2_wins += instances;
+                } else {
+                    new_game_states
+                        .entry(new_state)
+                        .and_modify(|e| *e += instances)
+                        .or_insert(instances);
+                }
+            }
+        }
+        game_states = new_game_states;
+    }
+
+    p_1_wins.max(p_2_wins)
 }
 
 #[cfg(test)]
@@ -105,19 +217,19 @@ mod tests {
         );
     }
 
-    //  #[test]
-    //  fn part_2_small_test() {
-    //      assert_eq!(
-    //          find_largest_magnitude_from_file("./inputs/day-18-input-test.txt").unwrap(),
-    //          3993
-    //      );
-    //  }
-    //
-    //  #[test]
-    //  fn part_2_test() {
-    //      assert_eq!(
-    //          find_largest_magnitude_from_file("./inputs/day-18-input.txt").unwrap(),
-    //          4673
-    //      );
-    //  }
+    #[test]
+    fn part_2_small_test() {
+        assert_eq!(
+            eval_dirac_game_from_file("./inputs/day-21-input-test.txt").unwrap(),
+            444356092776315_u128
+        );
+    }
+
+    #[test]
+    fn part_2_test() {
+        assert_eq!(
+            eval_dirac_game_from_file("./inputs/day-21-input.txt").unwrap(),
+            49975322685009_u128
+        );
+    }
 }
